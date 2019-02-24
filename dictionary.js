5d6a13e4eef0b09abe582e369b3059747def54d7
Ultranerd25 <43807857+Ultranerd25@users.noreply.github.com>
2019-02-24 14:49:56 -0500

    Add files via upload

--- dictionary.js
+++ dictionary.js
@@ -29,12 +29,20 @@ descface.push("mouth");
 descface.push("head");
 descface.push("ears");
 descface.push("nose");
+//assigning images to variables
+const anime_eyes = './Anime/anime_eyes.png';
+const anime_nose = './Anime/anime_nose.png';
+const anime_mouth = './Anime/anime_mouth.png';
+const anime_ears = './Anime/anime_ears.png';
 //read person input
-function character_style() {
-	console.log("button works");
-	var txtname = document.getElementById("character_style").value;
-	txtname = txtname.toLowerCase()
-	console.log(txtname);
+function image_import(height,width,image) {
+  	var x = document.createElement("IMG");
+  	x.setAttribute("src", image);
+  	x.setAttribute("width", width);
+  	x.setAttribute("height", height);
+  	x.setAttribute("alt", "image not accessible");
+  	document.body.appendChild(x);
+  	console.log("Image is here")
 }
 function character_imagine() {
 	console.log("button works");
@@ -44,16 +52,19 @@ function character_imagine() {
 //	var txtstyle = document.getElementById("character_style").value;
 //	txtstyle = txtstyle.toLowerCase()
 //	console.log(txtstyle);
-}
-//assigning images to variables
-const anime_eyes = './Anime/anime_eyes.png'
-const anime_nose = './Anime/anime_nose.png'
-const anime_mouth = './Anime/anime_mouth.png'
-const anime_ears = './Anime/anime_ears.png'
-
-//searching for key words
-//for (const word of descBig) {
-//	if (text.includes(word + descface[0]))
-
-//};
-
+	for (const word of descaverage)
+	{
+		if (txtname.includes(word +' '+ descface[0]))
+		{
+			image_import(100,200,anime_eyes);
+	  	}
+	  	if (txtname.includes(word +' '+ descface[1]))
+		{
+			image_import(100,200,anime_nose);
+	  	}
+	  	if (txtname.includes(word +' '+ descface[2]))
+		{
+			image_import(100,200,anime_mouth);
+	  	}
+	 }
+};
