d369e4b051a94ed0ba5fb1943efb52197446f5fd
Ultranerd25 <43807857+Ultranerd25@users.noreply.github.com>
2019-02-24 13:28:00 -0500

    Delete hack.js

--- hack.js
+++
@@ -1,7 +0,0 @@
-
-function characterimagine() {
-	console.log("button works")
-	var txtName = document.getElementById("characterinput").value;
-	console.log(txtName)
- // var txtOutput = document.getElementById("txtOutput");
-}
